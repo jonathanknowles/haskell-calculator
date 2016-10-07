8920f90cb4c0d33a678f3649dc14476193026eb1
Jonathan Knowles <mail@jonathanknowles.net>
2016-10-07 20:07:15 +0800

    Add help to the web interface.

--- source/library/Calculator/WebInterface.hs
+++ source/library/Calculator/WebInterface.hs
@@ -14,6 +14,7 @@ import Calculator.Printing
 import Calculator.Types
 import Calculator.Value
 import Control.Applicative ((<$>), (<*>))
+import Data.Monoid ((<>))
 import Data.Text.Encoding (encodeUtf8)
 import Data.Text (Text)
 import Reflex
@@ -22,42 +23,88 @@ import Reflex.Dom
 
 import qualified Data.Text as T
 
-css = encodeUtf8 $ T.unlines
-    [ "* {font: 15pt sans-serif}"
+css = T.unlines
+    [ "* {font-size: 14pt; font-family: 'Liberation Sans', 'Lato', sans-serif}"
     , "body {background-color: #aaaaaa; padding: 1em; margin: 0em}"
-    , "table {border: solid 0.2em; text-align: center; padding: 0.2em}"
-    , "td {text-align: center; vertical-align: center}"
-    , "tr {padding: 0em; border: 0em; margin: 0em}"
+      -- Links
+    , "a:link {text-decoration: none}"
+    , "a:visited {text-decoration: none}"
+    , "a:hover {text-decoration: underline}"
+    , "a:active {text-decoration: underline}"
+      -- Paragraphs
+    , "p {margin: 0em 0em 1em 0em}"
+    , "ul {margin: 0.2em 0em 0.2em 1em}"
+      -- Tables
+    , "table {border: solid 0.2em; text-align: center; padding: 0.2em; border-radius: 0.4em}"
     , "table.add {border-color: #ff0000; background-color: #ff8888}"
     , "table.sub {border-color: #008800; background-color: #88ff88}"
     , "table.mul {border-color: #0000ff; background-color: #8888ff}"
     , "table.div {border-color: #bb00bb; background-color: #bb88bb}"
-    , "table.val {border-color: #444444; background-color: #ffffff}"
     , "table.neg {border-color: #008800; background-color: #88ff88}"
-    , "input {border: solid 0.2em; padding: 0.2em; width: 90%}"
+    , "table.val {border-color: #444444; background-color: #ffffff}"
+    , "table.val {padding: 0.2em 0.4em 0.2em 0.4em}"
+    , "td {text-align: center; vertical-align: center}"
+    , "tr {padding: 0em; border: 0em; margin: 0em}"
+      -- Inputs
+    , "input {border: solid 0.2em; border-radius: 0.4em; padding: 0.2em 0.4em 0.2em 0.4em; width: 90%}"
+    , "input:focus {outline: none}"
     , "input.valid {border-color: #444444}"
     , "input.empty {border-color: #444444}"
     , "input.error {border-color: #ff0000}"
-    , "div.result {padding-top: 1em}"
-    , "div.feedback {font-weight: bolder; padding-top: 0.2em}"
-    , "div.heading {padding-top: 0.8em; padding-bottom: 0.2em}"
-    , "div.value {border: solid 0.2em #444444; background-color: #ffffff; padding: 0.2em; width: 90%}"
+      -- Sections
+    , "div {padding: 0em; margin: 0em}"
+    , "div.result {padding-top: 0.4em}"
+    , "div.feedback {padding-top: 0.2em; font-style: italic; font-weight: bold}"
+    , "div.heading {padding-top: 0.4em; padding-bottom: 0.2em; font-weight: bold}"
+    , "div.value {border: solid 0.2em #444444; border-radius: 0.4em}"
+    , "div.value {background-color: #ffffff; padding: 0.2em 0.4em 0.2em 0.4em; width: 90%}"
     ]
 
-main = mainWidgetWithCss css $ el "div" $ do
+main = mainWidgetWithHead headSection $ do
+        introduction
         parseResult <- expressionInput
         maybeExpression <- holdDyn Nothing $ removeInvalidExpressions parseResult
         dyn $ maybeEvaluateExpression <$> maybeExpression
         pure ()
     where
         maybeEvaluateExpression = maybe
-            (pure ())
+            help
             evaluateExpression
         removeInvalidExpressions = fmapMaybe
             (maybeEither
                 (Just Nothing)
                 (const Nothing)
                 (Just . Just)) . updated
+
+headSection = do
+    elAttr "link" ( "rel"  =: "stylesheet" <>
+                    "type" =: "text/css"   <>
+                    "href" =: "https://fonts.googleapis.com/css?family=Lato" ) $ text ""
+
+    elAttr "style" ( "type" =: "text/css" ) $ text css
+
+introduction = el "div" $
+        el "p" $ do
+            text "A "
+            el "strong" $ text "calculator"
+            text " implemented in "
+            elAttr "a" ("href" =: "https://www.haskell.org/") $ text "Haskell"
+            text ", "
+            elAttr "a" ("href" =: "https://github.com/ghcjs/ghcjs") $ text "GHCJS"
+            text ", and "
+            elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-platform") $ text "Reflex"
+            text ". ("
+            el "strong" $
+                elAttr "a" ("href" =: "https://github.com/jonathanknowles/haskell-calculator") $ text "View source code"
+            text ")"
+
+help = elAttr "div" ("class" =: "help") $
+    el "p" $ do
+        text "This calculator supports:"
+        el "ul" $ do
+            el "li" $ text "addition, subtraction, multiplication and division"
+            el "li" $ text "natural numbers"
+            el "li" $ text "parentheses"
 
 expressionInput :: MonadWidget t m => m (Dynamic t (Maybe (Either Text UExp)))
 expressionInput = do
@@ -66,16 +113,17 @@ expressionInput = do
                                             & textInputConfig_attributes   .~ c
             let c = resultClass <$> r
                 r = maybeParse <$> _textInput_value t
-            elAttr "div" ("class" =: "feedback") $ dyn $ feedbackText <$> r
+            elAttr "div" ("class" =: "feedback") $ dyn $ feedback <$> r
         return r
     where
         maybeParse x = if T.null x then Nothing
                                    else Just $ parseUExp x
-        feedbackText =
-            text . maybeEither hardSpace id (const hardSpace)
+        feedback = text .
+            maybeEither hardSpace id (const hardSpace)
         resultClass = ("class" =:) . maybeEither "empty"
                                           (const "error")
                                           (const "valid")
+
 
 maybeEither :: c -> (a -> c) -> (b -> c) -> Maybe (Either a b) -> c
 maybeEither n l r = \case
@@ -89,7 +137,7 @@ maybeEither' = maybeEither Nothing (const Nothing) Just
 evaluateExpression :: MonadWidget t m => UExp -> m ()
 evaluateExpression e =
     elAttr "div" ("class" =: "result") $ do
-        elAttr "div" ("class" =: "heading") $ text "Expression:"
+        elAttr "div" ("class" =: "heading") $ text "Result:"
         elAttr "div" ("class" =: "value"  ) $ text $ pretty $ e
         elAttr "div" ("class" =: "heading") $ text "Value:"
         elAttr "div" ("class" =: "value"  ) $ text $ pretty $ eval $ e
