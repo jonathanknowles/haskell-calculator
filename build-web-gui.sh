4c7f6c1c9d36464c1a901191d104f4256f3fd23c
Jonathan Knowles <mail@jonathanknowles.net>
2017-04-18 11:12:55 +0800

    Add script to build the web-based graphical user interface in a single step.

---
+++ build-web-gui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HIGHLIGHT='\033[1;32m'
+HIGHLIGHT_END='\033[0m'
+
+stack --stack-yaml stack.yaml.ghcjs build
+OUTPUT=$(stack --stack-yaml stack.yaml.ghcjs path --local-install-root)/bin/calculator-gui.jsexe
+cp style/style.css "$OUTPUT"/
+printf "${HIGHLIGHT}Open the following file with your web browser to view the graphical user interface:${HIGHLIGHT_END}\n"
+echo "$OUTPUT"/"index.html"
+
