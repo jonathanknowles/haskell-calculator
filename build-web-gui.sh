8e5c1828e00630621f38d8a9a89496ac9f702df9
Jonathan Knowles <mail@jonathanknowles.net>
2017-07-27 14:11:55 +0800

    Rename file "stack.yaml.ghcjs" to "stack.ghcjs.yaml".

--- build-web-gui.sh
+++ build-web-gui.sh
@@ -3,9 +3,9 @@
 HIGHLIGHT='\033[1;32m'
 HIGHLIGHT_END='\033[0m'
 
-stack --stack-yaml stack.yaml.ghcjs setup
-stack --stack-yaml stack.yaml.ghcjs build
-OUTPUT=$(stack --stack-yaml stack.yaml.ghcjs path --local-install-root)/bin/calculator-gui.jsexe
+stack --stack-yaml stack.ghcjs.yaml setup
+stack --stack-yaml stack.ghcjs.yaml build
+OUTPUT=$(stack --stack-yaml stack.ghcjs.yaml path --local-install-root)/bin/calculator-gui.jsexe
 cp style/style.css "$OUTPUT"/
 printf "${HIGHLIGHT}Open the following file with your web browser to view the graphical user interface:${HIGHLIGHT_END}\n"
 echo "$OUTPUT"/"index.html"
