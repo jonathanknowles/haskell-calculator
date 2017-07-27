f1e07f5a590752b8b022892c549bca43810a3772
Jonathan Knowles <mail@jonathanknowles.net>
2017-07-27 14:07:46 +0800

    Add "stack setup" to build-web-gui.sh script.

--- build-web-gui.sh
+++ build-web-gui.sh
@@ -3,6 +3,7 @@
 HIGHLIGHT='\033[1;32m'
 HIGHLIGHT_END='\033[0m'
 
+stack --stack-yaml stack.yaml.ghcjs setup
 stack --stack-yaml stack.yaml.ghcjs build
 OUTPUT=$(stack --stack-yaml stack.yaml.ghcjs path --local-install-root)/bin/calculator-gui.jsexe
 cp style/style.css "$OUTPUT"/
